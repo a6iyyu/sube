6e0c57feca741dfb1dce8ded5fa771e1585ad8e2
a6iyyu <ffst4irs@gmail.com>
2025-01-09 22:24:14 +0700

    :art: Improve the structure folder and code.

--- client/src/common/website-meta.tsx
+++ client/src/common/website-meta.tsx
@@ -5,11 +5,19 @@ export const WebsiteMeta: React.FC<{ title: string, description: string }> = ({ 
   return (
     <HelmetProvider>
       <Helmet>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+        <meta property="og:image" content="/rafi-abiyyu-airlangga.jpeg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index" />
         <meta name="description" content={description} />
+        <meta name="og:title" content={`${title}`} />
+        <meta name="og:description" content={`${description}`} />
+        <meta name="og:image" content="/favicon.ico?url" />
+        <meta name="twitter:title" content={`${title}`} />
+        <meta name="twitter:description" content={`${description}`} />
+        <meta name="twitter:image" content="/favicon.ico?url" />
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico?url" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
--- client/vite.config.mts
+++ client/vite.config.mts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
-import remarkMDXFrontmatter from "remark-mdx-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPath from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    mdx({ remarkPlugins: [remarkFrontmatter, remarkMDXFrontmatter] }),
+    mdx({ remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] }),
     react(),
     tsconfigPath(),
   ],
